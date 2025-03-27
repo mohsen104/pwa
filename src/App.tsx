1f3481078003f496ce8a9e5e152be7d4baaecdc5
mohsen104 <m.karimvand.84@gmail.com>
2025-03-27 17:26:20 +0330

    style: enhance App layout and add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import InstallPWAButton from "./InstallPwa";
 
 function App() {
   return (
-    <div>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <img
+        src="https://a.storyblok.com/f/202591/2058x1267/04db8b5663/pwa.png"
+        alt=""
+        style={{ width: "100%", maxWidth: "300px" }}
+      />
       <InstallPWAButton />
     </div>
   );
